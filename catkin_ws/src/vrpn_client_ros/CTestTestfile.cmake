f81f1260961df0309615060bfc0f69a286bde990
AhmadAlmuhtadi <82093446+AhmadAlmuhtadi@users.noreply.github.com>
2022-05-10 18:15:04 +0400

    Add files via upload

---
+++ catkin_ws/src/vrpn_client_ros/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmad/catkin_ws/src
+# Build directory: /home/ahmad/catkin_ws/src/vrpn_client_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mav_comm-master/mav_comm")
+subdirs("gazebo_ros_demos/rrbot_control")
+subdirs("gazebo_ros_demos/rrbot_description")
+subdirs("gazebo_ros_demos/rrbot_gazebo")
+subdirs("mav_comm-master/mav_state_machine_msgs")
+subdirs("mav_comm-master/mav_system_msgs")
+subdirs("mav_comm-master/mav_msgs")
+subdirs("capstone_mavros")
+subdirs("mav_comm-master/mav_planning_msgs")
+subdirs("ardu_urdf_sandbox")
+subdirs("gazebo_ros_demos/gazebo_tutorials")
